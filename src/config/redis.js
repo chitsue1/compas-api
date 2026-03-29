15d1544101584b0fc7546140c0809909df5ddc6d
chitsue1 <lgvaramadze3@gmail.com>
2026-03-29 16:46:26 +0400

    fix: mock redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,13 @@
 const client = {
   get: async () => null,
   setEx: async () => null,
+  del: async () => null,
   connect: async () => {},
   on: () => {},
 };
-const connect = async () => {};
+
+const connect = async () => {
+  console.log('Redis disabled - using mock');
+};
+
 module.exports = { client, connect };
